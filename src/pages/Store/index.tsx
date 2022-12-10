96514ef3d6f64267f429f4c33b91f785de4c52fd
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2022-12-10 17:49:58 -0300

    feat: add navbar from store page

--- src/pages/Store/index.tsx
+++ src/pages/Store/index.tsx
@@ -1,9 +1,25 @@
+import { BsFillHandbagFill } from "react-icons/bs";
+import { FaUserAlt } from "react-icons/fa";
+
 import * as S from "./styles";
 
 export const Store: React.FC = () => {
   return (
     <S.Container>
-      <div></div>
+      <S.Navbar>
+        <S.Row>
+          <S.Logo>GIFTS.GG</S.Logo>
+          <S.ActionsContainer>
+            <S.BtnCart>
+              <BsFillHandbagFill size={17} />
+            </S.BtnCart>
+            <S.BtnCart id="logout">
+              <FaUserAlt size={17} />
+            </S.BtnCart>
+            <label htmlFor="logout">Log out</label>
+          </S.ActionsContainer>
+        </S.Row>
+      </S.Navbar>
     </S.Container>
   );
 };
--- src/pages/Store/styles.ts
+++ src/pages/Store/styles.ts
@@ -3,3 +3,58 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
 `;
+
+export const Navbar = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 30px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1200px;
+  width: 100%;
+`;
+
+export const Logo = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.COLORS.BLACK};
+  font-family: "Inter", sans-serif;
+`;
+
+export const ActionsContainer = styled.div`
+  display: flex;
+
+  label {
+    font-size: 15px;
+    font-weight: bold;
+    color: ${({ theme }) => theme.COLORS.BLACK};
+    margin-top: 8px;
+    margin-left: 10px;
+    cursor: pointer;
+  }
+`;
+
+export const BtnCart = styled.button`
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  background-color: ${({ theme }) => theme.COLORS.BLUE};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-left: 15px;
+  cursor: pointer;
+
+  svg {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    margin-top: -3px;
+    margin-right: -1px;
+  }
+`;
