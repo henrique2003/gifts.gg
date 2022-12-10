893882047fe20b75ea6545ef65de1b490c730f5c
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2022-12-10 13:24:55 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { ThemeProvider } from "styled-components";
 
+// Packages
 import theme from "./theme";
 import GlobalStyle from "./theme/GlobalStyle";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Components
 import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/loja",
+    element: <Store />,
+  },
+]);
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
---
+++ src/pages/Store/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import * as S from "./styles";
+
+export const Store: React.FC = () => {
+  return (
+    <S.Container>
+      <div></div>
+    </S.Container>
+  );
+};
